75ee1cc5372d5de1094bbb3270599de0536ad4ac
lizdenhup <lizdenhup@gmail.com>
2017-08-18 13:34:32 -0500

    built search action

--- weather_api/app/controllers/api/v1/forecasts_controller.rb
+++ weather_api/app/controllers/api/v1/forecasts_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ForecastsController < ApplicationController
 
     def search
-        
+        result = ForecastService.new.get_forecast(params[:query])
+        render :plan => result, status: 200, content_type: 'application/json'
     end 
+    
 end 
--- weather_api/config/routes.rb
+++ weather_api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       #/api/v1/search
-      get '/search' to: 'forecast#search'
+      get '/search', to: 'forecast#search'
     end 
   end 
 end
