3970fd946b9b16bca881be890418dff3705d83d7
lizdenhup <lizdenhup@gmail.com>
2017-08-18 14:16:24 -0500

    get request working sans caching

--- weather_api/app/controllers/api/v1/forecasts_controller.rb
+++ weather_api/app/controllers/api/v1/forecasts_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::ForecastsController < ApplicationController
 
     def search
         result = ForecastService.new.get_forecast(params[:query])
-        render :plan => result, status: 200, content_type: 'application/json'
+        # byebug 
+        # result is returning the desired information even though Postman is not
+        render :json => result, status: 200, content_type: 'application/json'
     end 
-    
+
 end 
--- weather_api/app/services/forecast_service.rb
+++ weather_api/app/services/forecast_service.rb
@@ -4,17 +4,11 @@ class ForecastService
         cache_key = zipcode
         apikey = ENV['WUNDERGROUND_API_KEY']
         api_url = "http://api.wunderground.com/api/#{apikey}/forecast/q/#{cache_key}.json"
-        Rails.cache.fetch("#{cache_key}", expires_in: 1.hours) {
-          @response = Faraday.get(api_url)
+        # Rails.cache.fetch("#{cache_key}", expires_in: 1.hours) {
+          @response = Faraday.get("#{api_url}")
+          JSON.parse(@response.body)
           # not ideal - if resp is bad it still gets cached...
-          if @response.success? 
-            @response.body
-          end 
-        }
+        # }
     end
-    
-    def parse_response
-        JSON.parse(self.response)
-    end 
     
 end 
