0732b94b4b64531c76e90cb2d9e143f39a541bf7
lizdenhup <lizdenhup@gmail.com>
2017-08-18 15:53:11 -0500

    playing around with http-cache

--- weather_api/app/services/forecast_service.rb
+++ weather_api/app/services/forecast_service.rb
@@ -1,15 +1,40 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'faraday/http_cache'
+require 'active_support/logger'
+
 class ForecastService
-    
+    #not caching as desired - logger says uncacheable. some setting is wrong 
     def get_forecast(zipcode)
-        cache_key = zipcode
         apikey = ENV['WUNDERGROUND_API_KEY']
-        api_url = "http://api.wunderground.com/api/#{apikey}/forecast/q/#{cache_key}.json"
-        # Rails.cache.fetch("#{cache_key}", expires_in: 1.hours) {
-          @response = Faraday.get("#{api_url}")
-        byebug 
-          JSON.parse(@response.body)
-          # not ideal - if resp is bad it still gets cached...
-        # }
-    end
-    
+        client = Faraday.new('http://api.wunderground.com/api/') do |builder|
+            builder.use :http_cache, store: Rails.cache, logger: ActiveSupport::Logger.new(STDOUT)
+            builder.adapter Faraday.default_adapter
+        end
+
+        5.times do |index|
+            started = Time.now
+            puts "Request ##{index + 1}"
+            response = client.get('#{apikey}/forecast/q/#{zipcode}')
+            finished = Time.now
+            remaining = response.headers['X-RateLimit-Remaining']
+            limit = response.headers['X-RateLimit-Limit']
+        
+            puts "  Request took #{(finished - started) * 1000} ms."
+            puts "  Rate limits: remaining #{remaining} requests of #{limit}."
+        end
+    end 
 end 
+    
+    # def get_forecast(zipcode)
+    #     cache_key = zipcode
+    #     apikey = ENV['WUNDERGROUND_API_KEY']
+    #     api_url = "http://api.wunderground.com/api/#{apikey}/forecast/q/#{cache_key}.json"
+    #     # Rails.cache.fetch("#{cache_key}", expires_in: 1.hours) {
+    #       @response = Faraday.get("#{api_url}")
+    #       JSON.parse(@response.body)
+    #       # not ideal - if resp is bad it still gets cached...
+    #     # }
+    # end    
+# end 
