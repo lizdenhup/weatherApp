484ebf1b4457fc09a1c814bf9b5b0dfcc2a4507c
lizdenhup <lizdenhup@gmail.com>
2017-08-17 20:27:02 -0500

    caching API resp. using :memory_store as cache store for now

---
+++ weather_api/app/models/forecast.rb
@@ -0,0 +1,19 @@
+class Forecast < ApplicationRecord
+
+    def self.get_forecast
+        cache_key = zipcode
+        apikey = ENV['WUNDERGROUND_API_KEY']
+        api_url = "http://api.wunderground.com/api/#{apikey}/forecast/#{cache_key}.json"
+        Rails.cache.fetch("#{cache_key}/forecast", expires_in: 1.hours) {
+          @response = Faraday.get(api_url)
+          # not ideal if resp is bad it still gets cached...
+          if @response.success? 
+            @response.body
+          end 
+        }
+    end
+    
+    def self.parse_response
+        JSON.parse(self.response)
+    end 
+end 
