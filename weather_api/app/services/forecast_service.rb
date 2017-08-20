815ac7692c4f5e6dc062965750cb6770b19cee73
lizdenhup <lizdenhup@gmail.com>
2017-08-19 19:52:02 -0500

    HTTP cache configured to keep cached results fresh for 1 hr

--- weather_api/app/services/forecast_service.rb
+++ weather_api/app/services/forecast_service.rb
@@ -1,24 +1,37 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'faraday_middleware'
 require 'faraday/http_cache'
 require 'active_support/logger'
+
+class Faraday::OverrideCacheControl < Faraday::Middleware
+    def initialize(app, options = {})
+      super(app)
+      @cache_control = options[:cache_control]
+    end
+  
+    def call(env)
+      response = @app.call(env)
+      response.headers['Cache-Control'] = @cache_control
+      response
+    end
+end 
 
 class ForecastService
     #not caching as desired - logger says uncacheable. some setting is wrong 
     def get_forecast(zipcode)
         apikey = ENV['WUNDERGROUND_API_KEY']
-        client = Faraday.new('http://api.wunderground.com') do |builder|
+        @client = Faraday.new do |builder|
             builder.use :http_cache, store: Rails.cache, logger: ActiveSupport::Logger.new(STDOUT)
-            builder.response :json, :content_type => /\bjson$/
             builder.adapter Faraday.default_adapter
+            builder.use Faraday::OverrideCacheControl, cache_control: 'public, max-age=3600'
         end
 
         5.times do |index|
             started = Time.now
             puts "Request ##{index + 1}"
-            byebug 
-            response = client.get("/api/#{apikey}/forecast/q/#{zipcode}.json")
+            response = @client.get("http://api.wunderground.com/api/#{apikey}/forecast/q/#{zipcode}.json")
             finished = Time.now
             remaining = response.headers['X-RateLimit-Remaining']
             limit = response.headers['X-RateLimit-Limit']
